91578d267138025a0f40f3b07d2edd2bdba70519
Oleg Benets <oleg.benets@web.de>
2024-07-01 19:55:21 +0200

    added material design player and cards

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss',
+})
+export class GameComponent {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game = new Game();
+
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game);
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  pickCard() {
+    if (!this.pickCardAnimation) {
+      const card = this.game.stack.pop();
+      this.currentCard = card !== undefined ? card : '';
+      this.pickCardAnimation = true;
+      console.log(this.currentCard);
+      console.log(this.game);
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
+}
