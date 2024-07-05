699fdd7a4de2eaebb26490e1d16c63c1a0218f6d
Oleg Benets <oleg.benets@web.de>
2024-07-05 13:48:21 +0200

    removed environment

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {};
-const firebaseConfig = {
-    apiKey: "AIzaSyCD7sFHnHK0Mch4R3Kgt82QB6XmV1RIrHg",
-    authDomain: "ring-of-fire-f4d79.firebaseapp.com",
-    projectId: "ring-of-fire-f4d79",
-    storageBucket: "ring-of-fire-f4d79.appspot.com",
-    messagingSenderId: "636005028840",
-    appId: "1:636005028840:web:d1422fb9b6d339bf0d4a5d"
-  };
